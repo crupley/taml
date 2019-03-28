071f25a7607339898e10e56a143460b610f06660
Chris Rupley <crupley@salesforce.com>
2019-03-28 14:29:41 -0700

    add app framework

---
+++ taml/src/main/scala/com/crupley/taml/TamlApp.scala
@@ -0,0 +1,7 @@
+package com.crupley.taml
+
+object TamlApp {
+  def main(args: Array[String]): Unit = {
+    println("hello taml!")
+  }
+}
---
+++ taml/src/test/scala/com/crupley/taml/TamlAppTest.scala
@@ -0,0 +1,10 @@
+package scala.com.crupley.taml
+
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TamlAppTest extends FlatSpec {
+
+}
