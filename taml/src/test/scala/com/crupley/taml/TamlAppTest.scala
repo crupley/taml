8ffa9d731e0289f26bc21e6b62682a8b7ed62d97
Chris Rupley <crupley@salesforce.com>
2019-03-28 15:53:22 -0700

    update test

--- taml/src/test/scala/com/crupley/taml/TamlAppTest.scala
+++ taml/src/test/scala/com/crupley/taml/TamlAppTest.scala
@@ -1,10 +1,15 @@
-package scala.com.crupley.taml
+package com.crupley.taml
 
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
-import org.scalatest.junit.JUnitRunner
+import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class TamlAppTest extends FlatSpec {
 
+  behavior of "TamlAppTest"
+
+  it should "Effortlessly Train" in {
+    true
+  }
 }
